5c0465f89a30046af10fcfff85a5f713112c1a25
Wendy Wilgus <96785281+wendywilgus@users.noreply.github.com>
2022-10-06 21:21:52 -0400

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,52 +1,103 @@
 // Const inquirer Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 //Array of questions for user input
 const questions = [ 
     {type: 'input',
      name: 'title',
-     message: 'What would you like to use for the title of your project?'},
+     message: 'What would you like to use for the title of your project?',
+     validate: titleInput =>    {
+        if (titleInput) {
+            return true;
+        } else  {
+            console.log('You must enter a title!');
+            return false;
+        }
+     }
+    },
+
     {type: 'input',
-     name: "motivation",
-     message: "What was your motivation for building this project?"},
+     name: "username",
+     message: "What is your Github username?"},
+
     {type: 'input',
-     name: "reason",
-     message: "Why did you build this project? (Note: the answer is not because it was a homework assignment.)",},
+     name: "email",
+     message: "What is your email address?"},
+
+     {type: 'input',
+     name: "deployed",
+     message: "What is the address for the deployed application?"},
+
     {type: 'input',
-     name: "problem",
-     message: "What problem does it solve?",},
-    {type: 'input',
-     name: "learned",
-     message: "What did you learn?",},
-    {type: 'input',
-     name: "extra",
-     message: "What makes your project stand out?",},
+     name: "description",
+     message: "Please describe yoru project. You may want to include the motivation, the goals or problems that it solves, what you learned, or why this project stands out.",
+     validate: descriptionInput =>    {
+        if (descriptionInput) {
+            return true;
+        } else  {
+            console.log('You must include a project description!');
+            return false;
+        }
+     }
+    },
+
     {type: 'input',
      name: 'installation',
      message: 'What npm commands are needed to run this application?',},
+
     {type: 'input',
      name: 'usage',
      message: 'What is the repo for?  How can it be used?',},
-     {type: 'input',
+
+    {type: 'checkbox',
      name: 'license',
-     message: 'What license will you be using for this project? If none, press enter.',},
+     message: 'What license will you be using for this project?', choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'Unlicensed',],
+     validate: licenseInput =>    {
+        if (licenseInput) {
+            return true;
+        } else  {
+            console.log('You must chose a license!');
+            return false;
+        }
+     }
+    },
+
      {type: 'input',
      name: 'contributors',
      message: 'How can others contribute to this repo? Are there any rule that they need to abide by?',},
+
      {type: 'input',
      name: 'testing',
      message: 'What commands should be run to do tests?',},
+
      {type: 'input',
      name: 'questions',
      message: 'How can people ask questions about this repo?',},
 ];
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+//Function to write README file
+function writeToFile(fileName, data) {
+    const content = `# ${response.title}
+    ##### [$(response.username)] [Email] (${response.email})[Deployed Application]
+    
+    ##Table of Contents
+    * [Description](#Description)
+    * [Installation](#Install)
+    * [Usage](#Usage)
+    * [References](#References)
+    * [License](#License)
+    * [Contributors](#Contributors)
+    * [Testing](#Testing)
+    * [Questions](#Questions)`
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(answers => {console.log(answers)})
+    inquirer.prompt(questions).then(answers => {console.log(answers);
+        writeToFile();})
 }
 
 // Function call to initialize app
-init();
+init()
